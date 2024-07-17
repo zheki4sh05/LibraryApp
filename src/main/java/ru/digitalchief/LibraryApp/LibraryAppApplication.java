fd27556db0927b0b2e053b77f279bb37bd325123
evgluzedev <evgeniy.shostak.04@mail.ru>
2024-07-17 13:43:48 +0300

    Initial commit

---
+++ src/main/java/ru/digitalchief/LibraryApp/LibraryAppApplication.java
@@ -0,0 +1,13 @@
+package ru.digitalchief.LibraryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/digitalchief/LibraryApp/LibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.digitalchief.LibraryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
